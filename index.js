0d24bbc71781a7dd7d005ea00eaeedc0e47b9abf
Glitch (guaynab) <none>
2021-12-10 19:24:42 +0000

    yass

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const enmap = require('enmap')
-const log = require("log")
 const config = require("./config.json")
+
 const fastify = require("fastify")({
   logging: true
 })
+
 fastify.register(require("point-of-view"), {
   engine: {
     ejs: require("ejs"),
   },
 })
+
 const accounts = new enmap({
   name: "accounts",
   fetchAll: true,
   dataDir: "./.data"
 })
 
-if (config.frontend) require("./src/frontend/index.js")(fastify);
+fastify.register(require('./src/api/index.js'), { prefix: '/database' })
+
+if (config.frontend) require("./src/frontend/index.js")(fastify)
 
 fastify.listen(8000, (err, address) => {
-  if (err) log.error(err)
-  log.info("Server has started")
+  if (err) console.error(err)
+  console.log("Server has started")
 })
